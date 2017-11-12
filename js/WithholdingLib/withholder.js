29120ed6c7d4fecd894ca33a57535298d165e6a7
Sean Darcy <darcys22@gmail.com>
2017-11-12 13:30:20 +1100

    First commit

---
+++ js/WithholdingLib/withholder.js
@@ -0,0 +1,84 @@
+/*
+* EstimatorJS 0.0.1 - Australian Tax Payable Estimator
+* Copyright (c) 2017 Sean Darcy (Darcys22@gmail.com)
+* Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+* THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+(function (name, context, definition) {
+  if (typeof module !== 'undefined' && module.exports) { module.exports = definition(); }
+  else if (typeof define === 'function' && define.amd) { define(definition); }
+  else { context[name] = definition(); }
+})('Withholder', this, function () {
+  'use strict';
+
+  var Withholder = function (options) {
+    var nativeForEach, nativeMap;
+    nativeForEach = Array.prototype.forEach;
+    nativeMap = Array.prototype.map;
+
+    this.each = function (obj, iterator, context) {
+      if (obj === null) {
+        return;
+      }
+      if (nativeForEach && obj.forEach === nativeForEach) {
+        obj.forEach(iterator, context);
+      } else if (obj.length === +obj.length) {
+        for (var i = 0, l = obj.length; i < l; i++) {
+          if (iterator.call(context, obj[i], i, obj) === {}) return;
+        }
+      } else {
+        for (var key in obj) {
+          if (obj.hasOwnProperty(key)) {
+            if (iterator.call(context, obj[key], key, obj) === {}) return;
+          }
+        }
+      }
+    };
+
+    this.map = function(obj, iterator, context) {
+      var results = [];
+      // Not using strict equality so that this acts as a
+      // shortcut to checking for `null` and `undefined`.
+      if (obj == null) return results;
+      if (nativeMap && obj.map === nativeMap) return obj.map(iterator, context);
+      this.each(obj, function(value, index, list) {
+        results[results.length] = iterator.call(context, value, index, list);
+      });
+      return results;
+    };
+
+    if (typeof options == 'object'){
+      //this.hasher = options.hasher;
+    } else if(typeof options == 'function'){
+      //this.hasher = options;
+    }
+  };
+
+  Withholder.prototype = {
+
+    calculateTaxWithholding: function (taxableIncome, year) {
+      return 5000;
+
+		},
+
+    round: function(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+		}
+
+  };
+
+
+  return Withholder;
+
+});
---
+++ js/main.js
@@ -0,0 +1,60 @@
+var withholder = new Withholder();
+
+$('#TaxableIncomeInput').on('input', function () {
+    window.taxableIncome = stripwhitecommas($(this).val());
+    window.FY = stripwhitecommas($('#FinancialYearInput').val());
+    calculate();
+});
+$('#FinancialYearInput').on('input', function () {
+    window.FY = stripwhitecommas($(this).val());
+    calculate();
+});
+$('#settingsModal').on('hidden.bs.modal', function () {
+    editSettings();
+});
+
+function editSettings() {
+}
+
+function calculate() {
+  $("#TaxableIncomeOutput").html(moneyNumber(window.taxableIncome));
+  var withholding = withholder.calculateTaxWithholding(window.taxableIncome, window.FY);
+
+  $("#WithholdingVal").html(moneyNumber(withholding));
+
+  
+}
+
+function stripwhitecommas(str) {
+  if (!str || 0 === str.length) {
+    return str
+  } else {
+    return str.toString().replace(/[\s,]+/g,'').trim()
+  }
+}
+
+function moneyNumber(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function formatcomma(element) {
+  return element.toString().replace(/ /g,'').replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
+
+function main() {
+  window.now = moment();
+  if (window.now.month() < 6) {
+    window.now.set('year', now.year() -1);
+  }
+  window.now.set('month', 5);
+  window.now.set('date', 30);
+  window.endFY = moment(window.now);
+  window.startFY = moment(window.now.subtract(1, 'years').add(1,'days'));
+  $("#FinancialYearInput").val(window.endFY.format("YYYY"));
+  $("#FinancialYearInput").attr({"max": window.endFY.format("YYYY")});
+
+  window.taxableIncome = 50000;
+  window.FY = 2017;
+  calculate();
+}
+main();
